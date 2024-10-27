65660c5685668c2705a29066758a6e05293dcfbb
elvanagriawan <elvanawan@gmail.com>
2024-10-27 21:09:01 +0700

    update menggunakan fetch

--- movie.js
+++ movie.js
@@ -1,37 +1,76 @@
-$("#button-search").on('click',function(){
+// menggunakan Jquery
 
-$.ajax({
-  url: "http://www.omdbapi.com/?apikey=557c821a&s="+$(".input-movie").val(),
-  success: (result) => {
-    const movies = result.Search;
-    let card = "";
-    movies.forEach((m) => {
-      card += showCard(m)
-    });
-    $(".movie-container").html(card);
+// $("#button-search").on('click',function(){
+// $.ajax({
+//   url: "http://www.omdbapi.com/?apikey=557c821a&s="+$(".input-movie").val(),
+//   success: (result) => {
+//     const movies = result.Search;
+//     let card = "";
+//     movies.forEach((m) => {
+//       card += showCard(m)
+//     });
+//     $(".movie-container").html(card);
 
 
-    //tombol detail
-    $(".modal-button-info").on("click", function(){
-      $.ajax({
-      url:"http://www.omdbapi.com/?apikey=557c821a&i="+ $(this).data("imdbid"),
-        success: mi =>{
-          console.log(mi)
-          const movieInfo= showInfo(mi)
-            $(".modal-body-info").html(movieInfo);
-        },
-        error:(e)=>{
-          console.error('apalagi nieeh errornya '+e.response1)
-        }
-      })
-    });
-  },
+//     //tombol detail
+//     $(".modal-button-info").on("click", function(){
+//       $.ajax({
+//       url:"http://www.omdbapi.com/?apikey=557c821a&i="+ $(this).data("imdbid"),
+//         success: mi =>{
+//           console.log(mi)
+//           const movieInfo= showInfo(mi)
+//             $(".modal-body-info").html(movieInfo);
+//         },
+//         error:(e)=>{
+//           console.error('apalagi nieeh errornya '+e.response1)
+//         }
+//       })
+//     });
+//   },
 
-  error: (e) => {
-    console.error("kumaha ieu tehh eror wae"+e.response);
-  },
-});
+//   error: (e) => {
+//     console.error("kumaha ieu tehh eror wae"+e.response);
+//   },
+// });
+// menggunakan Jquery
 
+
+// fetch API
+//memunculkan card
+const tombol = document.querySelector('#button-search');
+
+tombol.addEventListener('click',function(){
+  const inputKeyword = document.querySelector('.input-movie');
+  fetch('http://www.omdbapi.com/?apikey=557c821a&s='+inputKeyword.value)
+  .then(response => response.json())
+  .then(response =>{
+    const movies = response.Search;
+    let card = '';
+    movies.forEach(m => {card += showCard(m)})
+    const cardContainer = document.querySelector(".movie-container")
+    cardContainer.innerHTML=card;
+
+//menampilkan kotak detail
+
+    const buttonInfo = document.querySelectorAll('.modal-button-info')
+    buttonInfo.forEach(btn => {
+    btn.addEventListener('click' , function(){
+      const imdbID = this.dataset.imdbid;
+      fetch('http://www.omdbapi.com/?apikey=557c821a&i=' + imdbID).then(response => response.json())
+        .then(response => { 
+        const detailBox =showInfo(response)
+        const modalBody = document.querySelector('.modal-body-info')
+        modalBody.innerHTML = detailBox;
+    })  
+  })
+})
+//menampilkan kotak detail 
+
+
+  })
+})
+
+// fetch API
 
 function showCard(m){
  return `<div class="col-md-4 my-3">
@@ -40,9 +79,7 @@ function showCard(m){
                     <div class="card-body">
                     <h5 class="card-title">${m.Title}</h5>
                     <p class="card-text text-muted">${m.Year}</p>
-                    <button type="button" class="btn btn-primary modal-button-info" data-bs-toggle="modal" data-bs-target="#modalBox" data-imdbid="${m.imdbID}">
-  show more
-</button>
+                    <button type="button" class="btn btn-primary modal-button-info" data-bs-toggle="modal" data-bs-target="#modalBox" data-imdbid="${m.imdbID}">more info</button>
                     </div>
                 </div>
                 </div>`;
@@ -66,4 +103,4 @@ function showInfo(m){
               </div>
             </div>`;
 }
-})
+// })
